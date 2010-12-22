1aff89f318ddcd2abfcb33acf100e7c1bc978ae5
Mark Harrison <mark@mivok.net>
2010-12-22 14:30:30 -0500

    Activate metrics command
    (not working currently - needs more api support)
    The API doesn't have a way to detect which metrics are available and which are
    not (because they don't exist any more on the agent side) - we need this to be
    able to detect which metrics to activate.

---
+++ module/activate_metrics.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+__cmdname__ = 'enable_metrics'
+__cmdopts__ = 'e'
+
+class Module(object):
+    def __init__(self, api, account):
+        self.api = api
+        self.account = account
+
+    def command(self, opts, check_id):
+        """List inactive metrics for a check, and optionally enable them
+
+        Options:
+            -e - enable the metrics
+        """
+        rv = self.api.list_checks()
+        check = None
+        for c in rv:
+            if str(c['check_id']) == check_id:
+                check = c
+        if not check:
+            print "Check %s not found\n" % check_id;
+            return
+        rv = self.api.list_metrics(check_id = check_id)
+        to_enable = []
+        already_enabled = []
+        print "Disabled Metric List for check %s (%s)" % (
+            check_id, check['name'])
+        for metric in sorted(rv):
+            if metric['enabled'] == False:
+                print "    %s" % metric['name']
+                print metric
+                to_enable.append(metric['name'])
+            else:
+                already_enabled.append(metric['name'])
+        if not to_enable:
+            print "    No disabled metrics"
+
+        if ('-e', '') in opts:
+            # Enable metrics here
+            self.api.edit_check_bundle(
+                bundle_id=check['bundle_id'],
+                metric_name = already_enabled + to_enable)
