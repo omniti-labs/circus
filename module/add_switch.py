b710633eee7bad5b662c96ed59870f41d08a6829
Mark Harrison <mark@mivok.net>
2010-12-28 14:49:17 -0500

    Remove graph code from the add_switch command

--- module/add_switch.py
+++ module/add_switch.py
@@ -1,5 +1,5 @@
 __cmdname__ = "add_switch"
-__cmdopts__ = "cg"
+__cmdopts__ = ""
 
 import logging
 import sys
@@ -37,10 +37,7 @@ class Module(object):
         self.community = community
         self.port_count = port_count
         self.friendly_name = friendly_name
-        if ('-c', '') in opts or not opts:
-            self.add_check()
-        if ('-g', '') in opts or not opts:
-            self.add_graphs()
+        self.add_check()
 
     def add_check(self):
         for i in range(1, int(self.port_count)):
@@ -64,23 +61,4 @@ class Module(object):
                 logging.debug("%20s = %s" % (k, params[k]))
 
             logging.debug("Adding check bundle")
-            #rv = self.api.add_check_bundle(**params)
-            # TODO - get check ID and store it for adding graphs
-
-    def add_graphs(self):
-        # TODO - move this into a separate command for adding graphs
-        # plan (from task post):
-        # search for checks with a certain name (regex filter?), create the
-        # graph with that check ID and the relevant metrics (we can use
-        # json/python dicts for the graph template itself and pass it almost
-        # directly). We just need to make sure that the relevant metrics are
-        # available.
-        t = graphutil.Template("switch")
-        check_ids = {1: 1234, 2:1235} # TODO - Get real check IDs
-        params = {
-            "switch_name": self.friendly_name,
-        }
-        for c in check_ids:
-            params['check_id'] = check_ids[c]
-            params['port'] = c
-            print t.sub(params)
+            rv = self.api.add_check_bundle(**params)
