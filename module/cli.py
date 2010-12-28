21ba121b39cebd28cb24fb573ed8bc269f6f6b96
Mark Harrison <mark@mivok.net>
2010-12-28 14:40:02 -0500

    CLI module for testing/debugging

---
+++ module/cli.py
@@ -0,0 +1,21 @@
+__cmdname__ = 'cli'
+
+import code
+
+class Module(object):
+    def __init__(self, api, account):
+        self.api = api
+        self.account = account
+
+    def command(self, opts):
+        """Start a python interpreter with access to the Circonus API.
+
+        This allows you to run api commands interactively. Access the api with
+        self.api. For example: self.api.list_checks().
+
+        Look at the circonusapi module for information on accessing the API.
+        """
+        banner = "Circus interpreter (python cli)\n" \
+                "use self.api to access the circonus api"
+        code.interact(banner=banner, local=locals())
+
