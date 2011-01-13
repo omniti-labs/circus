73938ae0ae0247d3e3e0870caee87db26aaf070f
Mark Harrison <mark@mivok.net>
2011-01-13 18:01:00 -0500

    Commands to dump rules and checks in raw json

---
+++ src/circus/module/dump_rules.py
@@ -0,0 +1,22 @@
+__cmdname__ = 'dump_rules'
+
+import re
+import json
+
+import circonusapi
+import log
+
+class Module(object):
+    def __init__(self, api, account):
+        self.api = api
+        self.account = account
+
+    def command(self, opts):
+        """Dump all rules on an account in json format
+
+        The aim for this command is to be able to 'back up' rules for later
+        importing.
+        """
+        rules = self.api.list_rules()
+        # Hack to prettify the json
+        print json.dumps(rules, indent=4)
--- src/circus/module/list_checks.py
+++ src/circus/module/list_checks.py
@@ -1,7 +1,8 @@
 __cmdname__ = 'list_checks'
-__cmdopts__ = 'la'
+__cmdopts__ = 'lar'
 
 import re
+import json
 
 class Module(object):
     def __init__(self, api, account):
@@ -14,6 +15,7 @@ class Module(object):
         Options:
             -l - Long listing (include extra information)
             -a - Include inactive and deleted checks also
+            -r - Output raw json suitable for backing up
 
         Arguments:
             pattern --  A regex to filter which checks are returned
@@ -29,18 +31,21 @@ class Module(object):
                     filtered_checks.append(c)
         else:
             filtered_checks = rv
-        print "Check List for %s" % self.account
-        for check in sorted(filtered_checks):
-            check_active = ''
-            if check['active'] == 'false':
-                check_active = ' (inactive)'
-            if check['active'] == 'deleted':
-                check_active = ' (deleted)'
-            print "    %s%s" % (check['name'], check_active)
-            if ('-l', '') in opts:
-                print "        Agent: %s, Type: %s" % (check['agent'],
-                                                   check['type'])
-                print "        Bundle Id: %s, Check Id: %s" % (
-                    check['bundle_id'], check['check_id'])
-                print "        Last modified by: %s on %s" % (
-                    check['last_modified_by'], check['last_modified'])
+        if ('-r', '') in opts:
+            print json.dumps(filtered_checks, indent=4)
+        else:
+            print "Check List for %s" % self.account
+            for check in sorted(filtered_checks):
+                check_active = ''
+                if check['active'] == 'false':
+                    check_active = ' (inactive)'
+                if check['active'] == 'deleted':
+                    check_active = ' (deleted)'
+                print "    %s%s" % (check['name'], check_active)
+                if ('-l', '') in opts:
+                    print "        Agent: %s, Type: %s" % (check['agent'],
+                                                    check['type'])
+                    print "        Bundle Id: %s, Check Id: %s" % (
+                        check['bundle_id'], check['check_id'])
+                    print "        Last modified by: %s on %s" % (
+                        check['last_modified_by'], check['last_modified'])
